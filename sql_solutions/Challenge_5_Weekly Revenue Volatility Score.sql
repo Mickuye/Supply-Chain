9506d571311ff7d6729ab3bca3961f43956093df
Mickuye <mickuye13@gmail.com>
2025-08-18 15:14:01 +0100

    Challenge_5_Weekly Revenue Volatility Score.sql

--- sql_solutions/Challenge_5_Weekly Revenue Volatility Score.sql
+++ sql_solutions/Challenge_5_Weekly Revenue Volatility Score.sql
@@ -1,6 +1,32 @@
+WITH table1 AS (
+    SELECT 
+        year, 
+        week, 
+        location, 
+        ROUND(SUM(totalrevenue), 0) AS revenue
+    FROM supply_chain3
+    GROUP BY year, week, location
+), 
+table2 AS (
+    SELECT
+        COUNT(*) AS num_weeks,
+        location, 
+        ROUND(AVG(revenue), 0) AS average,
+        ROUND(STDDEV_POP(revenue), 0) AS revenue_volatility,
+        CASE
+            WHEN ROUND(STDDEV_POP(revenue), 0) <= 15000 THEN 'Stable 👍'
+            WHEN ROUND(STDDEV_POP(revenue), 0) BETWEEN 15001 AND 30000 THEN 'Moderate 👌'
+            WHEN ROUND(STDDEV_POP(revenue), 0) > 30000 THEN 'Volatile 👎'
+        END AS volatility_flag
+    FROM table1
+    GROUP BY location
+    ORDER BY location
+)
 SELECT 
-  Location, 
-  ROUND(STDDEV_POP(TotalRevenue),2) AS Revenue_volatility,
-  Rank() OVER(ORDER BY STDDEV_POP(TotalRevenue) DESC) AS Rank_no
-FROM supply_chain2
-GROUP BY Location;
+    num_weeks, 
+    location, 
+    revenue_volatility, 
+    volatility_flag,
+    RANK() OVER (ORDER BY revenue_volatility DESC) AS rank_num
+FROM table2;
+
